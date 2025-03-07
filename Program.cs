9d855c62789ea8fe95dcd546d7da7e66d36934ce
Pavan021 <pavanpanni008@gmail.com>
2025-03-07 23:41:36 +0530

    If else statement with Console inputs/outputs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -17,7 +18,7 @@ namespace MyRecapProject
             //Variables
 
             // number based
-            int a = 10, b = 20, c = 30;
+            /*int a = 10, b = 20, c = 30;
             Console.WriteLine("ID of A : {0}, B: {1}, C: {2}", a, b, c);
             Console.WriteLine(int.MinValue);
             Console.WriteLine(int.MaxValue);     //Shortcuts: CW tab
@@ -40,11 +41,11 @@ namespace MyRecapProject
             decimal Weight = 75.3M;
             Console.WriteLine("Weight is {0}", Weight);
             Console.WriteLine(decimal.MinValue);
-            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MaxValue); */
 
             // Text based 
 
-            char Lastname = 'C';
+           /* char Lastname = 'C';
             string FirstName = "Pavan";
 
             Console.WriteLine("May name is :" + FirstName + Lastname);     
@@ -53,12 +54,12 @@ namespace MyRecapProject
             Console.WriteLine("Expected salary is :" + ExpSalary);
 
             bool isGenz = true;
-            //Console.WriteLine("GenZ :{0}", isGenz);
+            //Console.WriteLine("GenZ :{0}", isGenz); */
 
 
             // TypeCasting 
 
-            var Age = "32";
+            /*var Age = "32";
             int MyAge = Convert.ToInt32(Age);
             long HerAge = Convert.ToInt64(Age);
             double DoubleAge = Convert.ToDouble(Age);
@@ -76,11 +77,41 @@ namespace MyRecapProject
 
             num1 = 13;            //Reassigning Value 
             remainder = num1 % num2;
-            Console.WriteLine(remainder);
+            Console.WriteLine(remainder);  */
+
+
+            // Console Input/Output
+
+            Console.WriteLine("What is your name :");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your age :");
+            //int age = Console.ReadLine();                        // throw error as ReadLine() returns string
+            var ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+            //Console.WriteLine("Hi "+ name + ", your age is "+ age); 
+
+
+            // If else statement
+
+            if (age <= 0 || age >= 150)
+                Console.WriteLine("Invalid Age!!......");
+            else
+            {
+                if (age >=18 && age <= 25)
+                    Console.WriteLine("your age is between 18 and 25");
+                else if (age >=25)
+                    Console.WriteLine("You're 25 or older ");
+            }
 
 
 
-            Console.ReadLine();
+
+
+
+
+                Console.ReadLine();
 
 
         }
